860a1545408a2824f6a216f9daf4bf47c921f8cd
Mohammad hossein jadidi <jadidi.hosein@gmail.com>
2019-06-05 20:38:25 +0800

    add http client example

--- MicroService/Example.Service/Adapter/ExampleAdapter.cs
+++ MicroService/Example.Service/Adapter/ExampleAdapter.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
 using Service.Common.Models;
 
 namespace Example.Service.Adapter
 {
     public class ExampleAdapter : IExampleAdapter
     {
-        public ExampleAdapter()
+        private readonly IHttpClientFactory _clientFactory;
+        public ExampleAdapter(IHttpClientFactory clientFactory)
         {
+            _clientFactory = clientFactory;
         }
 
         public SampleModel getSampleModel()
@@ -18,6 +23,26 @@ namespace Example.Service.Adapter
                 Id = 1,
                 Name = "Sample Object"
             };
+        }
+
+        public async Task<SampleModel> HTTPClientExample()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get,
+           "https://api.binance.com/api/v1/ticker/24hr?symbol=");
+
+            var client = _clientFactory.CreateClient();
+
+            var response = await client.SendAsync(request);
+            if (response.IsSuccessStatusCode)
+            {
+                var results = await response.Content
+                    .ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<SampleModel>(results);
+            }
+            else
+            {
+                throw new Exception("faild to retrive data from external point");
+            }
         }
     }
 }
---
+++ MicroService/Service.Common/Models/Constants/SampleConstant.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Service.Common.Models.Constants
+{
+    public static class SampleConstant
+    {
+      public const int Sample1 = 1;
+      public const int Sample2 = 2;
+      public const int Sample3 = 3;
+    }
+}
